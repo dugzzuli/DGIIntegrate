36a97d80781a883fa77ec4c70e87745b5f47a14b
dguking <bingwei2642@qq.com>
2021-11-02 13:03:30 +0800

    集成DGI以及一些其他的算法

---
+++ layers/__init__.py
@@ -0,0 +1,8 @@
+from .gcn import GCN
+from .gat import GAT
+from .readout import AvgReadout
+from .discriminator import Discriminator
+from .attention import Attention
+from .attention import AttentionNoALL
+from .AutoEncoder import AutoEncoder
+
